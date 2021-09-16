996baa3180d566a0ba1e74099639c9bf843a24ea
Warona Mogolwane <mogolwanew@gmail.com>
2021-09-16 20:42:49 +0200

    Cards added to the home page with different goals v2

--- app/src/main/java/com/run/ultimate_fitness/MainActivity.java
+++ app/src/main/java/com/run/ultimate_fitness/MainActivity.java
@@ -1,6 +1,8 @@
 package com.run.ultimate_fitness;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -32,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        getSupportActionBar().hide();
 
     }
 
