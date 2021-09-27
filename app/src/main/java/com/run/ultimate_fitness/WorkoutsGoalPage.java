7a357dd1efb86d581d993c863b89c499ab0d910f
Warona Mogolwane <mogolwanew@gmail.com>
2021-09-27 17:09:39 +0200

    Workouts goal UI added (Warona)

---
+++ app/src/main/java/com/run/ultimate_fitness/WorkoutsGoalPage.java
@@ -0,0 +1,48 @@
+package com.run.ultimate_fitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+public class WorkoutsGoalPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workouts_goal_page);
+    }
+
+    public void clickSummerBody(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle("Workout Goal")
+                .setMessage("You have decided to Refine your Silhouette")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                    }
+                }).show();
+/*
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.ok, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+
+ */
+    }
+
+    public void clickGainMuscle(View view) {
+    }
+
+    public void clickLoseWeight(View view) {
+    }
+
+    public void clickLoseExtraWeight(View view) {
+    }
+}
