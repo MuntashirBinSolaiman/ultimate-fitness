49a33590037e26f6bb7dfa247c7fc67b9b498a8b
nmom0001 <nmom0001@student.monash.edu>
2021-09-29 13:39:05 +0200

    Added Water Package (Water Tracker Activity, DOB & Model), Added Water Layout, Added Water drawable

---
+++ app/src/main/java/com/run/ultimate_fitness/water/Water_Tracker_Activity.java
@@ -0,0 +1,209 @@
+package com.run.ultimate_fitness.water;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.run.ultimate_fitness.R;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class Water_Tracker_Activity extends AppCompatActivity {
+    private int cups_of_water = 0;
+    //private int id = 0;
+    private ProgressBar progressBar;
+    private TextView text_view_progress;
+    private TextView tvDate;
+    private ListView lv_view;
+    private WaterModel waterModel;
+   // private EditText et_size_of_cup;
+    private ArrayAdapter waterArrayAdapter;
+    private Water_Tracker_DOBHelper waterTrackerDobHelper;
+
+    Button insert, update, delete, view, open_calendar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.water_tracker_activity_main);
+
+        text_view_progress = findViewById(R.id.text_view_progress);
+        progressBar = findViewById(R.id.progress_bar);
+        open_calendar = findViewById(R.id.open_calendar);
+        tvDate = findViewById(R.id.tv_date);
+       // et_size_of_cup = findViewById(R.id.et_size_of_cup);
+        insert = findViewById(R.id.add_to_database);
+        view = findViewById(R.id.view_table);
+        delete = findViewById(R.id.delete_table);
+        lv_view = findViewById(R.id.lv_view);
+
+        waterTrackerDobHelper = new Water_Tracker_DOBHelper(Water_Tracker_Activity.this);
+
+        //ShowWaterEntryOnListView(dobHelper);
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        open_calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        Water_Tracker_Activity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, month);
+                        calendar.set(Calendar.DAY_OF_MONTH, day);
+                        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+                        TextView tvDate = (TextView) findViewById(R.id.tv_date);
+                        tvDate.setText(currentDateString);
+
+                        month = month + 1;
+                        String date = day +"/"+ month +"/" + year;
+                        tvDate.setText(date);
+                    }
+                }, year,month,day);
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                WaterModel waterModel;
+
+                try {
+                    waterModel = new WaterModel(-1, tvDate.getText().toString(),
+                            Integer.parseInt(text_view_progress.getText().toString()));
+                    Toast.makeText(Water_Tracker_Activity.this, waterModel.toString(), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(Water_Tracker_Activity.this, "Error creating water entry", Toast.LENGTH_SHORT).show();
+                    waterModel = new WaterModel(-1, "error", 0);
+                }
+                Water_Tracker_DOBHelper waterTrackerDobHelper = new Water_Tracker_DOBHelper(Water_Tracker_Activity.this);
+                Boolean success = waterTrackerDobHelper.addWaterProgress(waterModel);
+                //ShowWaterEntryOnListView(dobHelper);
+                Toast.makeText(Water_Tracker_Activity.this, "Success" + " " + success, Toast.LENGTH_SHORT).show();
+                waterArrayAdapter = new ArrayAdapter<WaterModel>(Water_Tracker_Activity.this,
+                        android.R.layout.simple_list_item_1, waterTrackerDobHelper.getInfo());
+                lv_view.setAdapter(waterArrayAdapter);
+            }
+
+
+
+        });
+
+
+        lv_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                WaterModel clickedEntry = (WaterModel) parent.getItemAtPosition(position);
+                waterTrackerDobHelper.deleteOne(clickedEntry);
+                waterArrayAdapter = new ArrayAdapter<WaterModel>(Water_Tracker_Activity.this,
+                        android.R.layout.simple_list_item_1, waterTrackerDobHelper.getInfo());
+                lv_view.setAdapter(waterArrayAdapter);
+                Toast.makeText(Water_Tracker_Activity.this, "Entry  " + " " + clickedEntry + " " + "Deleted", Toast.LENGTH_SHORT).show();
+
+            }
+            });
+
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Water_Tracker_DOBHelper waterTrackerDobHelper = new Water_Tracker_DOBHelper(Water_Tracker_Activity.this);
+                //Toast.makeText(MainActivity.this, info.toString(), Toast.LENGTH_SHORT).show();
+
+                //Display
+                //ShowWaterEntryOnListView(dobHelper);
+
+
+
+                waterArrayAdapter = new ArrayAdapter<WaterModel>(Water_Tracker_Activity.this,
+                        android.R.layout.simple_list_item_1, waterTrackerDobHelper.getInfo());
+
+
+
+                if(waterArrayAdapter.getCount() == 0){
+
+                    lv_view.setAdapter(waterArrayAdapter);
+
+                }else{
+                    Toast.makeText(Water_Tracker_Activity.this, "Entry empty, Please add Progress", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dateD = tvDate.getText().toString();
+                waterModel = new WaterModel(-1, tvDate.getText().toString(),
+                        Integer.parseInt(text_view_progress.getText().toString()));
+
+                Water_Tracker_DOBHelper waterTrackerDobHelper =
+                        new Water_Tracker_DOBHelper(Water_Tracker_Activity.this);
+
+                Boolean checkdelete = waterTrackerDobHelper.deleteAll(waterModel);
+
+                if(checkdelete)
+                    Toast.makeText(Water_Tracker_Activity.this, "Progress Deleted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(Water_Tracker_Activity.this, "Progress Not Deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    /*private void ShowWaterEntryOnListView(DOBHelper dobHelper) {
+        waterArrayAdapter = new ArrayAdapter<WaterModel>(MainActivity.this,
+                android.R.layout.simple_list_item_1, dobHelper.getInfo());
+    }*/
+
+    public void drinkWater(View view) {
+        if (cups_of_water <= 99) {
+            cups_of_water++;
+            String waterDrank = String.valueOf(cups_of_water);
+            String waterProgress = waterDrank + "";
+            text_view_progress.setText("" + cups_of_water );
+            System.out.println(waterProgress);
+            updateProgressBar();
+        }
+    }
+
+    public void remove_drinkWater(View view) {
+        if (cups_of_water >= 1) {
+            cups_of_water--;
+            String waterDrank = String.valueOf(cups_of_water);
+            String waterProgress = waterDrank + "";
+            text_view_progress.setText("" + cups_of_water );
+            System.out.println(waterProgress);
+            updateProgressBar();
+        }
+    }
+
+    public void updateProgressBar() {
+        progressBar.setProgress(cups_of_water);
+    }
+}
---
+++ app/src/main/java/com/run/ultimate_fitness/water/Water_Tracker_DOBHelper.java
@@ -0,0 +1,138 @@
+package com.run.ultimate_fitness.water;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Water_Tracker_DOBHelper extends SQLiteOpenHelper {
+    public static final String WATER_TABLE = "WATER_TABLE";
+    public static final String COLUMN_DATE = "DATE";
+    public static final String COLUMN_CUPS_OF_WATER = "CUPS_OF_WATERC";
+    //private static final String COLUMN_SIZE_OF_CUP = "SIZE_OF_CUPC";
+    public static final String COLUMN_ID = "ID";
+
+    public Water_Tracker_DOBHelper(@Nullable Context context ) {
+        super(context, "water_tracker_db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String createTableStatement = "CREATE TABLE " + WATER_TABLE + " " +
+                "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_DATE + " TEXT, "
+                + COLUMN_CUPS_OF_WATER + "INT)";// + COLUMN_SIZE_OF_CUP + " INT)";
+        db.execSQL(createTableStatement);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + WATER_TABLE);
+    }
+
+    public Boolean addWaterProgress(com.run.ultimate_fitness.water.WaterModel waterModel) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_CUPS_OF_WATER, waterModel.getCups_of_waterC());
+        cv.put(COLUMN_DATE, waterModel.getDate());
+        //cv.put(COLUMN_SIZE_OF_CUP, waterModel.getSize_of_cup());
+
+        long insert = db.insert(WATER_TABLE, null, cv);
+        if (insert == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean deleteAll(com.run.ultimate_fitness.water.WaterModel waterModel){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String queryString = "DELETE FROM " + WATER_TABLE + " WHERE " + COLUMN_ID + " = " + waterModel.getId();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+
+        if (cursor.moveToNext()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean deleteOne(com.run.ultimate_fitness.water.WaterModel waterModel){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String queryString = "DELETE FROM " + WATER_TABLE + " WHERE " + COLUMN_ID + " = " + waterModel.getId() +  " AND " ;
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+
+        if (cursor.moveToFirst()){
+            return true;
+        }else {
+                return false;
+        }
+        }
+
+
+    public boolean updateData(com.run.ultimate_fitness.water.WaterModel waterModel){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_CUPS_OF_WATER, waterModel.getCups_of_waterC());
+        cv.put(COLUMN_DATE, waterModel.getDate());
+        //cv.put(COLUMN_SIZE_OF_CUP, waterModel.getSize_of_cup());
+
+        long insert = db.update(WATER_TABLE, cv, "id=?", new String[] {COLUMN_ID});
+        if (insert == -1) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    public List<com.run.ultimate_fitness.water.WaterModel> getInfo() {
+       List<com.run.ultimate_fitness.water.WaterModel> returnList = new ArrayList<>();
+       //Getting Data
+        String queryString = "SELECT * FROM " + WATER_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+
+        if(cursor.moveToFirst()){
+            // if result true move
+            do{
+               int waterID = cursor.getInt(0);
+               String waterDate = cursor.getString(1);
+               int waterCups_of_water = cursor.getInt(2);
+               //int waterSize_of_cups = cursor.getInt(3);
+
+                com.run.ultimate_fitness.water.WaterModel newWaterEntry =
+                        new com.run.ultimate_fitness.water.WaterModel(waterID, waterDate, waterCups_of_water);
+               returnList.add(newWaterEntry);
+
+            } while (cursor.moveToNext());
+        }else {
+            //do something
+        }
+
+        cursor.close();
+        db.close();
+        return returnList;
+    }
+
+
+
+}
+
+
